c79b16e88afb59cb6aaa5eed771493ed376fb74a
benjdan <107645741+benjdan@users.noreply.github.com>
2025-12-17 22:19:58 +0800

    ADD: Initial Rest services
    - Added initial rest endpoint services
    - Added initial config and test cases

---
+++ src/main/java/com/voting/api/dto/AuthResponse.java
@@ -0,0 +1,53 @@
+/* --------------------------------------------
+ * (c) All rights reserved.
+ */
+package com.voting.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	private String email;
+	private String password;
+    private String name;
+    private Long userId;
+}
