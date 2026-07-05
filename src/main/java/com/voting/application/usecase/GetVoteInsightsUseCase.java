90969d9bbf9583569defe4fb91f33941f46556d4
benjdan <107645741+benjdan@users.noreply.github.com>
2026-07-05 17:02:03 +0800

    ADD: Add logging for vote insights generation
    Add logging for vote insights generation

--- src/main/java/com/voting/application/usecase/GetVoteInsightsUseCase.java
+++ src/main/java/com/voting/application/usecase/GetVoteInsightsUseCase.java
@@ -5,6 +5,8 @@ package com.voting.application.usecase;
 
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,11 +16,13 @@ import com.voting.application.service.AIService;
 @Service
 @AllArgsConstructor
 public class GetVoteInsightsUseCase {
-    
+    private static final Logger LOGGER = LogManager.getLogger(GetVoteInsightsUseCase.class);
+
 	@Autowired
     private AIService aiService;
     
     public String execute(Long voteId) {
+		LOGGER.debug("Generating vote insights for voteId={}", voteId);
         return aiService.generateVoteInsights(voteId);
     }
 }
