108633f90eb77a2b50ba1a803ace0c94d2c71118
benjdan <107645741+benjdan@users.noreply.github.com>
2026-07-06 12:37:02 +0800

    ADD: Add logging to GetVoteHistoryUseCase
    Add logging to GetVoteHistoryUseCase

--- src/main/java/com/voting/application/usecase/GetVoteHistoryUseCase.java
+++ src/main/java/com/voting/application/usecase/GetVoteHistoryUseCase.java
@@ -8,6 +8,8 @@ import com.voting.domain.valueobject.VoteRecord;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,11 +19,13 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class GetVoteHistoryUseCase {
-    
+    private static final Logger LOGGER = LogManager.getLogger(GetVoteHistoryUseCase.class);
+
 	@Autowired
     private BlockchainService blockchainService;
     
     public List<VoteRecord> execute(Long voteId) {
+		LOGGER.debug("Fetching vote history for voteId={}", voteId);
         return blockchainService.getVoteHistory(voteId);
     }
 }
