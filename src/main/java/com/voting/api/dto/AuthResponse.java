5dc3a6ae612f5d155b790a2568be699071cdde09
benjdan <107645741+benjdan@users.noreply.github.com>
2026-08-30 18:33:57 +0800

    UPD: Refactor AuthResponse
    Refactor AuthResponse

--- src/main/java/com/voting/api/dto/AuthResponse.java
+++ src/main/java/com/voting/api/dto/AuthResponse.java
@@ -14,38 +14,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AuthResponse {
     private String token;
-    public String getToken() {
-		return token;
-	}
-	public void setToken(String token) {
-		this.token = token;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public Long getUserId() {
-		return userId;
-	}
-	public void setUserId(Long userId) {
-		this.userId = userId;
-	}
 	private String email;
 	private String password;
     private String name;
